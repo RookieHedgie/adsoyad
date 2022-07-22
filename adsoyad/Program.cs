13a1ee091f3f7d85b3f6cee82f55e6a4ac639198
Faruk Yıldırım <farukyildirim7878@gmail.com>
2022-07-22 20:26:41 +0300

    adsoyad

---
+++ adsoyad/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace adsoyad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İsminizi Giriniz");
+            string isim = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz");
+            string soyisim = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + isim + " " + soyisim);
+        }
+    }
+}
+
+
